9e145f3236e9b7ca3c1d57a37dc84ec894f65f13
StephensJJ <stepsedwards@gmail.com>
2017-06-14 19:15:03 -0400

    added fitness frog start point

---
+++ src/FitnessFrog/Models/Activity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessFrog.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public enum ActivityType
+        {
+            Basketball = 1,
+            Biking = 2,
+            Hiking = 3,
+            Kayaking = 4,
+            PokemonGo = 5,
+            Running = 6,
+            Skiing = 7,
+            Swimming = 8,
+            Walking = 9,
+            Weightlifting = 10
+        }
+
+        public Activity(ActivityType activityType, string name = null)
+        {
+            Id = (int)activityType;
+            Name = name ?? activityType.ToString();
+        }
+    }
+}
---
+++ src/FitnessFrog/Models/Entry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FitnessFrog.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Activity")]
+        public int ActivityId { get; set; }
+
+        public Activity Activity { get; set; }
+
+        public double Duration { get; set; }
+
+        public IntensityLevel Intensity { get; set; }
+
+        public bool Exclude { get; set; }
+
+        public int MyProperty { get; set; }
+
+        [MaxLength(200, ErrorMessage = "The notes field cannot be longer than 200 characters")]
+        public string Notes { get; set; }
+
+        public enum IntensityLevel
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        public Entry()
+        {
+
+        }
+
+        public Entry(int id, int year, int month, int day, Activity.ActivityType activityType, 
+            double duration, IntensityLevel intensity = IntensityLevel.Medium, 
+            bool exclude = false, string notes = null)
+        {
+            Id = id;
+            Date = new DateTime(year, month, day);
+            ActivityId = (int)activityType;
+            Duration = duration;
+            Intensity = intensity;
+            Exclude = exclude;
+            Notes = notes;
+        }
+    }
+}
